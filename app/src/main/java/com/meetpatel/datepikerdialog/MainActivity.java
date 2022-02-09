4981e024cc2fd7d5fcaea07a41a6dbffddceeac9
patel meet <19bmiit0015@gmail.com>
2022-02-09 22:40:07 +0530

    DatePickerDialog

---
+++ app/src/main/java/com/meetpatel/datepikerdialog/MainActivity.java
@@ -0,0 +1,50 @@
+package com.meetpatel.datepikerdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvDate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDate = findViewById(R.id.tv_date);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(calendar.YEAR);
+        final int month = calendar.get(calendar.MONTH);
+        final int day = calendar.get(calendar.DAY_OF_MONTH);
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                                i1 = i1 + 1;
+                                String data = i2 + "/" + i1 + "/" + i;
+                                tvDate.setText(data);
+                            }
+                        },year,month,day
+                );
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+    }
+}
